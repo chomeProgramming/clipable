38bd019f7ca42c6065a786165460c218b81fa397
MTZGER <66328710+MTZGER@users.noreply.github.com>
2021-05-22 10:58:23 +0200

    finished uploading and getting video

--- index.js
+++ index.js
@@ -3,8 +3,32 @@ const exphbs = require("express-handlebars")
 const fs = require("fs")
 const path = require("path")
 const fileUpload = require("express-fileupload")
-const multer = require("multer")
 const { Pool, Client } = require("pg")
+const multer = require('multer');
+
+// const upload = multer({ dest: './uploads/' })
+const fileStorageEngine = multer.diskStorage({
+    destination: (req, file, cb) => {
+        if (file.mimetype.split("/")[0] == "video")
+            cb(null, './uploads/videos')
+        else
+            cb(null, './uploads/images')
+    },
+    filename: (req, file, cb) => {
+        db.query("SELECT uuid_generate_v4()", (err, result) => {
+            if (err)
+                console.log(err)
+
+            // cb(null, Date.now() + "--" + file.originalname)
+            cb(null, result.rows[0].uuid_generate_v4 + "." + file.originalname.slice(1+file.originalname.lastIndexOf(".")))
+            // cb(null, result.rows[0].uuid_generate_v4)
+        })
+    }
+})
+const upload = multer({ storage: fileStorageEngine })
+
+// multer example: https://www.youtube.com/watch?v=srPXMt1Q0nY
+// more info for multer in: multer_hint.txt
 
 const app = express()
 const PORT = process.env.PORT || 2662
@@ -19,33 +43,9 @@ const db = new Pool({
 const sqls = {
     start: fs.readFileSync("./sql/start.sql").toString(),
 }
-// const storage = multer.diskStorage({
-//     destination: (req, file, cb) => {
-//         cb(null, "./uploads")
-//     },
-//     filename: function(req, file, cb) {
-//         cb(null, Date.now() +
-//         path.extname(file.originalName))
-//     }
-// })
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, "uploads")
-    },
-    filename: (req, file, cb) => {
-        console.log("this is from the multer:")
-        console.log(file)
-        cb(null, Date.now() + path.extname(file.originalname))
-    }
-})
-const upload = multer({
-    storage: storage,
-    limits: { fieldSize: 10 * 1024 * 1024 }
-})
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(fileUpload())
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }))
 app.set("view engine", "handlebars")
@@ -61,7 +61,14 @@ app.use((req, res, next) => {
 })
 
 app.get("/", (req, res) => {
-    res.render("index")
+    db.query("SELECT id, caption, mimetype FROM videos;", (err, result) => {
+        if (err)
+            console.log(err.message)
+        res.render("index", {
+            title: "Clipable",
+            videos: result.rows
+        })
+    })
 })
 
 app.get("/post/", (req, res) => {
@@ -71,30 +78,37 @@ app.get("/create", (req, res) => {
     res.render("upload")
 })
 app.post("/upload", upload.single("video"), (req, res) => {
-    if (!req.files)
+    if (!req.file) {
+        console.log("Error: Not get file from multer")
         return res.render("not_found")
+    }
 
-    console.log("File: ", req.file)
-    res.json("Created successfully")
+    console.log(req.file)
+    const fileName = req.file.filename
+    db.query("INSERT INTO videos (id, mimetype, caption, video) VALUES ($1, $2, $3, $4);", [fileName.slice(0, fileName.lastIndexOf(".")), fileName.slice(fileName.lastIndexOf(".")+1), fileName, fs.readFileSync(`./uploads/videos/${fileName}`)], (err) => {
+        if (err)
+            console.log(err.message)
 
-    // console.log(req.body)
-    // console.log(req.files)
-    // const fileName = req.files.video.name
-    // db.query("INSERT INTO videos (mimetype, caption, video) VALUES ($1, $2, $3);", [fileName.slice(fileName.lastIndexOf(".")+1), fileName, req.files.video.data], (err) => {
-    //     if (err)
-    //         console.log(err.message)
-
-    //     res.redirect("/")
-    // })
-    // res.redirect("/")
+        res.redirect("/")
+    })
 })
-app.get("/videos/:video_id", (req, res) => {
+
+app.use("/", express.static(path.join(__dirname, "uploads"))) // later: check if exists in sql download it; else not_found
+app.get("/sql/videos/:video_id", (req, res) => {
     if (!req.params.video_id)
         return res.render("not_found")
 
-    // res.writeHead(200, {'Content-Type': 'video/mp4'});
-    // res.end(uploadedVideos[req.params.video_id].video)
-    res.json("Filtered")
+    db.query("SELECT video FROM videos WHERE id = $1", [req.params.video_id], (err, result) => {
+        if (err) {
+            console.log(err.message)
+            return res.render("not_found")
+        }
+        if (result.rowCount == 0)
+            return res.render("not_found")
+
+        res.writeHead(200, {'Content-Type': 'video/mp4'});
+        res.end(result.rows[0].video)  
+    })
 })
 
 app.get('/favicon.ico', (req, res) => {
--- sql/start.sql
+++ sql/start.sql
@@ -3,7 +3,8 @@ CREATE TABLE IF NOT EXISTS videos (
     id UUID DEFAULT uuid_generate_v4 () PRIMARY KEY,
     mimetype TEXT NOT NULL,
     caption TEXT NOT NULL,
-    video BYTEA NOT NULL
+    video BYTEA NOT NULL,
+    uploadtype TEXT NOT NULL DEFAULT 'video'
 );
 
 -- channel_id (foreign key)
